9d4e9d96b26c58f104d5c87e629c05a03d2766d2
Mavrrick <36817650+Mavrrick@users.noreply.github.com>
2025-06-24 11:00:43 -0400

    Update unifiIntegrationManagerOutbound-Webhook.groovy

--- unifiIntegrationManagerOutbound-Webhook.groovy
+++ unifiIntegrationManagerOutbound-Webhook.groovy
@@ -24,6 +24,7 @@ definition(
     description: "Unifi Integration Manager webhook Child app",
     category: "Convenience",
     parent: "Mavrrick:Unifi Integration Manager",
+    importUrl: "https://raw.githubusercontent.com/Mavrrick/Unifi/refs/heads/main/unifiIntegrationManagerOutbound-Webhook.groovy",
     iconUrl: "https://graph.api.smartthings.com/api/devices/icons/st.Weather.weather2-icn",
     iconX2Url: "https://graph.api.smartthings.com/api/devices/icons/st.Weather.weather2-icn"
 )
@@ -39,7 +40,7 @@ preferences {
 	}
 	section("Select capability of device use to detect activity") {
         input ("capabilityType", "enum", title: "Capability of the device to trigger Call to Unifi Alarm Manager.",
-           options:["Motion","Temperature","Contact Sensor"], submitOnChange: true)    
+           options:["Motion","Temperature","Contact Sensor"], submitOnChange: true) 
 	}   
     section("Device") {
         if (capabilityType == "Motion") {
@@ -50,6 +51,19 @@ preferences {
 		    input ("contact", "capability.contactSensor", required: false)
 	    }
     }
+        section("What trigger are you looking for") {
+        if (capabilityType == "Motion") {
+           input ("motionState", "enum", title: "Capability of the device to trigger Call to Unifi Alarm Manager.",
+           options:["inactive", "active","both"], submitOnChange: true)
+        } else if (capabilityType == "Temperature") {
+           input ("temperatureCompare", "enum", title: "Capability of the device to trigger Call to Unifi Alarm Manager.",
+           options:[">", "<","="], submitOnChange: true)
+           input ("tempValue", "number", title: "Please enter the number to validate against", required: false)
+        } else if (capabilityType == "Contact Sensor") {
+           input ("contactState", "enum", title: "Capability of the device to trigger Call to Unifi Alarm Manager.",
+           options:["open", "closed","both"], submitOnChange: true)
+        } 
+    }    
     section("Enter the Complete Webhook URL") {
         input ("url", "string", title: "Please past the entire Webhook URL from Unifi Alarm Manager", required: false)
     }
@@ -62,20 +76,54 @@ preferences {
 }
         
 def installed() {
-    if (temp) subscribe(temp, "temperature", callWebhook)
-    if (motion) subscribe(motion, "motion", callWebhook)
-    if (contact) subscribe(contact, "contact", callWebhook)
-    log.info("Installed Heater manager application")
+    log.info("Installed Unifi Outlook-Webhook")
+    subscribeTo()
 }
 
 def updated() {
 	unsubscribe()
-    if (temp) subscribe(temp, "temperature", callWebhook)
-    if (motion) subscribe(motion, "motion", callWebhook)
-    if (contact) subscribe(contact, "contact", callWebhook)
-    log.info("Updated Heater manager application")
+    log.info("Installed Unifi Outlook-Webhook")
+    subscribeTo()
 }
-     
+
+def subscribeTo() {
+    if (capabilityType == "Motion") {
+        if (motionState == "inactive") {
+            subscribe(motion, "motion.inactive", callWebhook)
+        } else if (motionState == "active") {
+            subscribe(motion, "motion.active", callWebhook)
+        } else {
+            subscribe(motion, "motion", callWebhook)
+        }
+    } else if (capabilityType == "Temperature") {
+        subscribe(temp, "temperature", tempHandler)
+    } else if (capabilityType == "Contact Sensor") {
+        if (contactState == "open") {
+            subscribe(contact, "contact.open", callWebhook)
+        } else if (contactState == "closed") {
+            subscribe(contact, "contact.closed", callWebhook)
+        } else {
+            subscribe(contact, "contact", callWebhook)
+        }
+    } 
+}
+
+def tempHandler(evt) {
+    if (temperatureCompare == ">") {
+        if (evt > tempValue) {
+            callWebhook (evt)
+        }
+    } else if (temperatureCompare == "<") {
+        if (evt < tempValue) {
+            callWebhook (evt)
+        } 
+    } else if (temperatureCompare == "=") {
+        if (evt == tempValue) {
+            callWebhook (evt)
+        } 
+    }
+}
+
      
 def callWebhook(evt) {
     log.debug "An Event took place"
